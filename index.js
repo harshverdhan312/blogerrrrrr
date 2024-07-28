81599b4511e850315e70d65f6cf8e3d8ec570555
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2024-07-28 09:06:59 +0530

    frontend pages addition

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/",(req,res)=>{
     res.render("index")
 })
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.get("/signup",(req,res)=>{
+    res.render("signup")
+})
+app.get("/readBlogs",(req,res)=>{
+    res.render("readBlogs")
+})
+app.get("/write",(req,res)=>{
+    res.render("writeBlogs")
+})
+app.get("/profile",(req,res)=>{
+    res.render("profile")
+})
+
 app.listen(3020,(req,res)=>{
     console.log(`Server running at: http://127.0.0.1:${3020}`);
 })
