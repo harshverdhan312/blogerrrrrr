c32da6556272d7e76270d48ffcb66940eb2689f2
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2024-08-15 06:23:31 +0530

    typo solved in name

---
+++ routes/healthcheck.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { healthcheck } = require("../controllers/healthcheck.controller");
+
+const router = Router();
+
+router.route('/').get(healthcheck);
+
+module.exports = {
+    router
+};
