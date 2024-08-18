451d837d3dee7349a0860e72c5097dbe9c440c59
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2024-08-18 08:12:52 +0530

    routes declared

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const userRouter = require("./routes/user.routes.js")
 const blogRouter = require("./routes/blog.routes.js")
 const healthcheckRouter = require("./routes/healthcheck.routes");
 const followRouter = require("./routes/follow.routes.js")
+const likeRouter = require("./routes/like.routes.js")
+const commentRouter = require("./routes/comment.routes.js")
 
 //routes declaration
 
@@ -26,6 +28,8 @@ app.use("/api/v1/users", userRouter)
 app.use("/api/v1/blogs", blogRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/follow",followRouter)
+app.use("/api/v1/like",likeRouter)
+app.use("/api/v1/comment",commentRouter)
 
 
 module.exports = {app}
